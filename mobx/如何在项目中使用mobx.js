f123c2e622a3b254712fb8c85eedd930cbcbaea8
baikaifa <1956841815@qq.com>
2020-02-02 09:52:39 +0800

    增加如何使用mobx

---
+++ mobx/如何在项目中使用mobx.js
@@ -0,0 +1,9 @@
+//1. cnpm i axios antd jquery mobx wd-domsize-monitor uglifyjs-webpack-plugin mobx-react react-dom react-loadable customize-cra react-app-rewired react-scripts element-theme-default  less  less-loader react-router react-router-dom react-scripts react-transition-group react-zmage  store swiper@3.4.2 video.js videojs-flash http-proxy-middleware  babel-plugin-import css-loader@1.0.0 -S
+//2. 添加config.overrides.js 
+//3. 修改package.json中scripts
+//   "scripts": {
+//     "start": "react-app-rewired start",
+//     "build": "react-app-rewired build",
+//     "test": "react-app-rewired test",
+//     "eject": "react-scripts eject"
+//   },
